50435cac88b2c521850df49d709a0c5f2e05a877
avianna-wynks <103884884+avianna-wynks@users.noreply.github.com>
2022-06-17 17:22:55 +0800

    avi

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,6 +1,12 @@
 function CreatePost() {
     return (
-        <p>CreatePost</p>
+        <div>
+        <h2>Add your design</h2>
+        <p>upload image</p>
+        <p>discription</p>
+        <p>style</p>
+        <p>cost</p>
+        </div>
     )
 }
 
