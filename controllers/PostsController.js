dd03c231ac46b51c699cecfd63685e0d3249365e
healthier-vitamins <88876449+healthier-vitamins@users.noreply.github.com>
2022-06-17 15:06:57 +0800

    amended .post entry

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
 })
 // create post
 //! i think needs to be router.post
-router.get("/create", (req, res) => {
+router.post("/create", (req, res) => {
     res.send("you'll create post here")
   })
 
