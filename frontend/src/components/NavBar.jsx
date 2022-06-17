12d7a4ba0e86652f8412dbddd2197ed003c6f04d
healthier-vitamins <88876449+healthier-vitamins@users.noreply.github.com>
2022-06-17 11:48:11 +0800

    react routes updated

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ function NavBar () {
     return (
         <>
         <Link to="/" >Home Page</Link>
-        <Link to="/credentials/:id">Sign Up/Login</Link>
+        <Link to="/sign-up/">Sign Up/Login</Link>
         <Link to="/profile/:id">Profile</Link>
         <Link to="/show-post/:id">onClick event (ShowPost)</Link>
         {/* if logged in */}
