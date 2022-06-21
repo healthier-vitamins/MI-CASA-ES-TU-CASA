0e8b9bff833486a1d8e1cb06074f5fb0a58efcdb
avianna-wynks <103884884+avianna-wynks@users.noreply.github.com>
2022-06-21 16:54:56 +0800

    kk

---
+++ frontend/src/components/showPost/DeleteModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function DeleteModal() {
+  return (
+    <div>
+      <p>are you sure you'd like to delete this post?</p>
+      <button>yes</button>
+      <button>no</button>
+    </div>
+  )
+}
+
+export default DeleteModal
--- frontend/src/pages/ShowPost.jsx
+++ frontend/src/pages/ShowPost.jsx
@@ -1,12 +1,14 @@
-import show from "./ShowPost.module.css"
-import { useState, useEffect } from "react"
-import ImageModal from "../components/showPost/ImageModal"
+import show from "./ShowPost.module.css";
+import { useState, useEffect } from "react";
+import ImageModal from "../components/showPost/ImageModal";
 import { useParams } from "react-router-dom";
-// import { Splide, SplideSlide } from '@splidejs/react-splide';
-
+import DeleteModal from "../components/showPost/DeleteModal";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css/skyblue";
 
 function ShowPost() {
   const [showModal, setShowModal] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false);
   const [imgId, setImgId] = useState("");
   const [thisPost, setThisPost] = useState({});
 
@@ -20,93 +22,104 @@ function ShowPost() {
     fetch(`/api/posts/${id}`)
       .then((response) => response.json())
       .then((data) => {
+        setThisPost(data?.data);
       });
   }, []);
 
-  console.log("k",thisPost)
+  const handleDelete = () => {
+    console.log("you're gonna be deleted :(");
+  };
 
-  const imgs = [
-      "https://i.imgur.com/woBZD95.jpeg",
-      "https://i.imgur.com/k8BaoR8.jpeg",
-      "https://i.imgur.com/woBZD95.jpeg",
-      "https://i.imgur.com/woBZD95.jpeg",
-      "https://i.imgur.com/k8BaoR8.jpeg"
-  ]
+  const handleEdit = () => {
+    console.log("haven't created this function :(");
+  };
 
-  return (
-    <div className={show.container}>
-      <div>
-        <ImageModal 
-        showModal={showModal} 
-        setShowModal={setShowModal}
-        imgId={imgId}
-         />
-      </div>
-      <div className={show.titlebar}>
-      {/* <p className="title"> title </p> */}
-      <a className={show.username} href="/profile/:id">{thisPost.username}</a>
-      </div>
-      <div className={show.imageswrapper}> 
-      {/* <Splide 
-          options={{  
-            perPage: 5,
-            arrows: true,
-            rewind : false,
-            drag: "free",
-            gap: "10rem",
-        }}
-      > */}
-            <div className={show.images}>
-            {imgs.map((i, index) => (
-            // <SplideSlide key={index}>
-                <img key={index}
-                src={i}
-                width={"280px"}
-                alt=""
-                onClick={handleclick => {setImgId(i)
-                  toggleModal()}}
-            />
-            ))}
-          {/* </SplideSlide> */}
-                </div>
-         {/* </Splide> */}
-         </div>
-      <div className={show.discription}>
-        <div className={show.dleft}>
-          <p>{thisPost.style}</p>
-          <p> total cost: {thisPost.const}</p>
+  // console.log("k",thisPost)
+
+  if (Object.keys(thisPost).length < 1) {
+    return "loading";
+  } else {
+    return (
+      <div className={show.container}>
+        <div>
+          <ImageModal
+            showModal={showModal}
+            setShowModal={setShowModal}
+            imgId={imgId}
+          />
         </div>
-        <div className={show.dright}>
-          <p>
-          {thisPost.description}
-          </p>
+        <div className={show.titlebar}>
+          <a className={show.username} href="/profile/:id">
+            {thisPost.username}
+          </a>
+          <p className={show.companyname}> {thisPost.company_name} </p>
         </div>
-      </div>
-      <div className={show.commentsandlikes}>
-        <div className={show.comments}>
-          <div className={show.commleft}>
-            comments:
-            <p> this is beautiful! </p>
-            <p>see all ### comments</p>
-            <form>
-              <textarea
-                type="text"
-                id="comment"
-                name="comment"
-                placeholder="add your comment"
-                // value={}
-                // onChange={}
-              />
-              <button> add </button>
-            </form>
+        <div className={show.imageswrapper}>
+          <Splide options={{
+            perPage: 2,
+            gap: "1rem",
+          }}>
+              {thisPost?.img.map((i, index) => (
+            <SplideSlide key={index}>
+                <img
+                  key={index}
+                  src={i}
+                  width={"280px"}
+                  height={"220px"}
+                  alt=""
+                  onClick={(handleclick) => {
+                    setImgId(i);
+                    toggleModal();
+                  }}
+                  />
+                  </SplideSlide>
+              ))}
+          </Splide>
+          <div className={show.images}></div>
+        </div>
+        <div className={show.discription}>
+          <div className={show.dleft}>
+            {/* <p>{thisPost.style}</p> */}
+            <p> total cost: {thisPost.cost}</p>
+          </div>
+          <div className={show.dright}>
+            <p>{thisPost.description}</p>
           </div>
         </div>
-        <div className={show.commright}>
-          <p>### people liked this post</p>
+        <div className={show.commentsandlikes}>
+          <div className={show.comments}>
+            <div className={show.commleft}>
+              comments:
+              <p> this is beautiful! </p>
+              <p>see all ### comments</p>
+              <form>
+                <textarea
+                  type="text"
+                  id="comment"
+                  name="comment"
+                  placeholder="add your comment"
+                  // value={}
+                  // onChange={}
+                />
+                <button> add </button>
+              </form>
             </div>
+          </div>
+          <div className={show.commright}>
+            <p>### people liked this post</p>
+            <p>like this post</p>
+            <p className={show.editdelete} onClick={handleEdit}>
+              edit this post
+            </p>
+            <p className={show.editdelete} onClick={handleDelete}>
+              delete this post
+            </p>
+            <DeleteModal />
+          </div>
         </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default ShowPost;
