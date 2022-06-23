68e3bb6b922194ac4a91f3312187db41feca7d1e
avianna-wynks <103884884+avianna-wynks@users.noreply.github.com>
2022-06-23 09:32:51 +0800

    back to top button

--- frontend/src/components/Index/PostCard.jsx
+++ frontend/src/components/Index/PostCard.jsx
@@ -1,21 +1,25 @@
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import card from "./PostCard.module.css"
+import { useAtom } from "jotai";
+import { userAtom } from "../../App.jsx";
 
 
 function PostCard({ post }) {
+  const [user, setUser] = useAtom(userAtom);
 
   return (
 
     <div>
       <Card className={card.card}>
-        <Card.Title className={card.title}> {post.username}</Card.Title>
-        {/* insert link to post when click on the image? */}
+      <Card.Title className={card.title}> 
+      <a className={card.titletext} href={`/profile/${post.username}/${user._id}`}>{post.username}</a>
+      </Card.Title>
       <Card.Img className={card.img} variant="top" src={post.img[0]} />
       <Card.Body>
         <Card.Text>
-        {post.description}
-        Style: {post.style}
+        {post.description} <br/><br/>
+        Style: {post.style} <br/>
         Cost: {post.cost}
         </Card.Text>
         <Link className={card.link} to={`/show-post/${post._id}`}>check this design</Link>
